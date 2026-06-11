928988dab0f9dcb4b606e94b858e8ebc00e6534d
Trong Nguyen <trongnp@gmail.com>
2026-06-12 01:02:45 +0700

    feat(frontend): create header for PC

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Header } from "./components/header/Header";
 
 export const metadata: Metadata = {
   title: "AITasker",
@@ -13,9 +14,12 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="vi"
     >
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
