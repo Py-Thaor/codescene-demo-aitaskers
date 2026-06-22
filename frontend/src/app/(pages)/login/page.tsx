6743a69f4db6c6023de0a0f855a361348798647c
Trong Nguyen <trongnp@gmail.com>
2026-06-22 19:20:11 +0700

    feat(login): add login form and API integration

--- frontend/src/app/(pages)/login/page.tsx
+++ frontend/src/app/(pages)/login/page.tsx
@@ -1,84 +1,86 @@
-import Link from "next/link";
-import { FaBuilding, FaUserTie } from "react-icons/fa6";
+"use client"
+
+import { BoxGroup1 } from "@/app/components/form/BoxGroup1";
+import { ButtonGroup2 } from "@/app/components/form/ButtonGroup2";
+import { useState } from "react";
 
 export default function Page() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e:any) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://localhost:8081/api/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      console.log(data);
+      localStorage.setItem("token", data.token);
+
+      localStorage.setItem(
+        "user",
+        JSON.stringify(data.user)
+      );
+      
+      window.location.href = "/";
+    } catch (error:any) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
   return (
     <>
       <div className="py-[60px] sm:min-h-[calc(100vh-121px-72px)] min-h-[calc(100vh-121px-60px)]">
         <div className="contain">
           <div className="bg-white rounded-[8px] border border-[#DEDEDE] px-[20px] py-[50px] max-w-[602px] w-full mx-[auto]">
             <h1 className="font-[700] text-[20px] text-black mb-[20px] text-center">
-              Vui lòng chọn vai trò để tiếp tục đăng nhập
+              Đăng nhập
             </h1>
-            <div className="grid gap-8 mt-10">
-              <Link
-                href="/user/login"
-                className="
-                  group
-                  border-2 border-gray-200
-                  rounded-2xl
-                  p-8
-                  hover:border-blue-600
-                  hover:shadow-xl
-                  transition-all
-                  duration-300
-                  text-center
-                "
-              >
-                <FaUserTie
-                  className="
-                    text-6xl
-                    text-blue-600
-                    mb-6
-                    mx-auto
-                    group-hover:scale-110
-                    transition
-                  "
-                />
-
-                <h3 className="text-2xl font-bold">
-                  AI Expert
-                </h3>
-
-                <p className="mt-3 text-gray-500">
-                  Find projects and connect with companies.
-                </p>
-              </Link>
-
-              <Link
-                href="/company/login"
-                className="
-                  group
-                  border-2 border-gray-200
-                  rounded-2xl
-                  p-8
-                  hover:border-blue-600
-                  hover:shadow-xl
-                  transition-all
-                  duration-300
-                  text-center
-                "
-              >
-                <FaBuilding
-                  className="
-                    text-6xl
-                    text-blue-600
-                    mb-6
-                    mx-auto
-                    group-hover:scale-110
-                    transition
-                  "
-                />
-
-                <h3 className="text-2xl font-bold">
-                  Company
-                </h3>
-
-                <p className="mt-3 text-gray-500">
-                  Hire AI experts and build your team.
-                </p>
-              </Link>
-            </div>
+            <form onSubmit={handleSubmit} action="" className="grid gap-x-[20px] gap-y-[15px]">
+              <BoxGroup1
+                name="username"
+                label="Email *"
+                id="emai"
+                type="email"
+                values={formData.username}
+                onChange={handleChange}
+              />
+              <BoxGroup1
+                name="password"
+                label="Mật khẩu *"
+                id="password"
+                type="password"
+                values={formData.password}
+                onChange={handleChange}
+              />
+              <ButtonGroup2
+                label="Đăng nhập"
+              />
+            </form>
           </div>
         </div>
       </div>
